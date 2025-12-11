6ba50e5d51222685b648fe6ed88ea559353be595
PC <PC@DESKTOP-EFQ18EP>
2025-12-11 20:38:32 +0300

    Добавьте файлы проекта.

---
+++ LabLibrary/Password.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace LabLibrary
+{
+    public static class Password
+    {
+        public static bool PasswordValidation(string password)
+        {
+            if (!string.IsNullOrEmpty(password) && password.Length >= 8 && password.Any(char.IsDigit) && Regex.IsMatch(password, "[A-Za-z]"))
+                return true;
+            return false;
+        }
+    }
+}
---
+++ TestProject/UnitTest1.cs
@@ -0,0 +1,77 @@
+﻿using LabLibrary;
+
+namespace TestProject
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Password_PasswordValidation_True()
+        {
+            string pass = "12345678W";
+
+            bool result = Password.PasswordValidation(pass);
+
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void Password_PasswordValidation_False_AllButShort()
+        {
+            string pass = "123Q";
+
+            bool result = Password.PasswordValidation(pass);
+
+            Assert.False(result);
+        }
+
+        [Fact]
+        public void Password_PasswordValidation_False_ShortAndNoLatSymbol()
+        {
+            string pass = "1234";
+
+            bool result = Password.PasswordValidation(pass);
+
+            Assert.False(result);
+        }
+
+        [Fact]
+        public void Password_PasswordValidation_False_ShortAndNoDigit()
+        {
+            string pass = "Pass";
+
+            bool result = Password.PasswordValidation(pass);
+
+            Assert.False(result);
+        }
+
+        [Fact]
+        public void Password_PasswordValidation_False_NoDigit()
+        {
+            string pass = "PasswordW";
+
+            bool result = Password.PasswordValidation(pass);
+
+            Assert.False(result);
+        }
+
+        [Fact]
+        public void Password_PasswordValidation_False_NoLatSymbol()
+        {
+            string pass = "1234567890122334";
+
+            bool result = Password.PasswordValidation(pass);
+
+            Assert.False(result);
+        }
+
+        [Fact]
+        public void Password_PasswordValidation_False_Nofing()
+        {
+            string pass = "";
+
+            bool result = Password.PasswordValidation(pass);
+
+            Assert.False(result);
+        }
+    }
+}
