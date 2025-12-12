319ae9c0481124ce54c156754a8b8a666bf96696
PC <PC@DESKTOP-EFQ18EP>
2025-12-12 17:43:54 +0300

    itwork?

---
+++ LabLibrary/testwork.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabLibrary
+{
+    public class testwork
+    {
+        public void IThinkThisWork()
+        {
+
+        }
+    }
+}
